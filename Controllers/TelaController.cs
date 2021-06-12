8cf38107e824af0a889bd77dac567298185bdd46
Roberto <rrrr.22.1993@gmail.com>
2021-06-11 21:21:31 -0400

    Examen 2

---
+++ Controllers/TelaController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ApiTela.Context;
+
+namespace ApiTela.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class TelaController : ControllerBase
+    {
+        private readonly AppDbContext context;
+        public TelaController(AppDbContext _context)
+        {
+            this.context = _context;
+        }
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            //Error handling Try-catch
+            return Ok(context.tela.ToList());
+        }
+
+    }
+}
---
+++ Models/Tela.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiTela.Models
+{
+    public class Tela
+    {
+        [Key]
+        public int id { get; set; }
+        public string marca { get; set; }
+        public string ancho { get; set; }
+        public string largo { get; set; }
+        public string tipo { get; set; }
+        public string color { get; set; }
+        public string pais { get; set; }
+        public bool estado { get; set; }
+    }
+}
